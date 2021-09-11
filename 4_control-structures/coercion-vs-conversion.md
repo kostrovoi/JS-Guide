5792b0867353485dd438800d8ffcff029d932743
Vitalii Marchenko <vitaly.marchenko77@gmail.com>
2021-09-11 18:14:13 +0300

    add scrimba arrays and sets

--- 4_control-structures/coercion-vs-conversion.md
+++ 4_control-structures/coercion-vs-conversion.md
@@ -1,3 +1,5 @@
 coerce - "convert without really converting".
 That means that JS tries to interpret some value (string, number, null) as a boolean
 JS generates a new boolean which is temporarily used in the comparison.
+
+To observe the closure you must Call function in different scope than where function was original defined.
---
+++ srimba/arrays_and_sets.js
@@ -0,0 +1,249 @@
+// JS неявно приводит  к строке все типы в ключах обЪекта
+
+const nums = {
+  1: 1,
+  true: true,
+};
+
+console.log(Object.keys(nums));
+
+// получаем массив ключей из строк
+// и тип "символ" тоже  приводится к строке
+// а объект Map не меняет тип ключа и оставляет как есть - str, num, boolean, symbol
+const map1 = new Map([
+  [1, 1],
+  [true, true],
+]);
+
+map1.set('key', 'value');
+
+console.log([...map1.keys()]);
+
+// с помощью метода set()  добавляем в Мапу пару  ключ - значение
+// Мапа мутирует и обновляется хоть и объявлена через const
+// с помощью ...spread оператора получаем массив ключей
+// типы переменных в ключах остались оригинальными
+// Мапа сохраняет последовательность ключей,  обычный объект этого не гарантирует
+// чтобы использовать объект как ключ , но не тянуть весь объект используют  WeakMap
+const user1 = { name: 'john' };
+const secretKey1 = 'asldjfalskdjf';
+
+const secretKeyMap = new WeakMap([[user1, secretKey1]]);
+
+const key = secretKeyMap.get(user1);
+console.log(key);
+
+// чтобы получить длину объекта - запрашиваем длину массива с ключами этого объекта
+Object.keys(nums).length;
+
+// у Мапы это метод size()
+
+const userMap = new Map([
+  ['name', 'john'],
+  ['verified', true],
+]);
+
+console.log(userMap.size);
+
+// работа с объктом это работа с его ключами
+// Массив:
+// работа с массивом это работа с его  индексами - с  упорядоченной, то есть последовательно проиндексированной коллекцией данных.
+// чекаем существование элемента в массиве
+
+const temperaturesNums = [69, 82, 73, 64]; // 0, 1, 2, 3; -1
+
+console.log(temperaturesNums.indexOf(50)); // -1 элемент отсутствует
+console.log(temperaturesNums.indexOf(50) > -1); // false отсутствие элемента  привели к булевому значению
+
+// второй метод  проверки элемента в массиве:
+console.log(temperaturesNums.includes(50)); //false
+
+// эти методы подходят для поиска в массиве состоящем из примитивных типов
+
+// Vitalii, [06.09.21 09:53]
+// чекаем существование элемента в массиве состоящем из объектов
+
+const temperatures = [
+  { degrees: 69, isRecordTemp: false },
+  { degrees: 82, isRecordTemp: true },
+  { degrees: 73, isRecordTemp: false },
+  { degrees: 64, isRecordTemp: false },
+];
+
+const resultAny = temperatures.some(
+  (temperature) => temperature.isRecordTemp === true
+);
+// true / false  так как мы смотрим в ключ  isRecordTemp значения которого булевые то сравнение с === true избыточно
+console.log(resultAny);
+
+// в методе some() вызываем коллбэк в котором итерируемся по каждому объекту, обозвав его temperature и смотрим в свойство объекта
+// проверяем есть ли хоть 1 элемент в массиве с такой парой ключ-значение temperature.isRecordTemp === true
+// при первом же true метод some()  прекращает  выполнение
+// иначе дойдёт до конца массива и вернёт false
+// Метод every() - чекнем все ли значения false в ключе isRecordTemp
+
+const result = temperatures.every((temperature) => !temperature.isRecordTemp); // true / false
+console.log(result);
+
+//  \!temperature.isRecordTemp  можно заменить для простоты на  temperature.isRecordTemp === false
+
+// indexof() includes() some() every() методы проверки существ-ия эл-та в массиве
+
+// методы итерации по массиву
+// Array.prototype.map()
+// Метод map не изменяет массив, для которого он был вызван (хотя функция callback может это делать).
+
+console.log(temperatures);
+
+const newTemps = temperatures.map((temperature) => {
+  temperature.isRecordTemp = true;
+  // если раскоментить строку выше , то она трансформирует начальный массив
+  // temperature.isHigh = true;
+  //  return temperature;
+  return temperature.degrees > 70
+    ? { ...temperature, isHigh: true }
+    : temperature;
+});
+console.log(newTemps);
+
+const tempsHigh = temperatures.map((temperature) =>
+  temperature.degrees > 70 ? { ...temperature, isHigh: true } : temperature
+);
+console.log(tempsHigh);
+
+// создаём новый массив  методом map()
+// метод итерации по массиву forEach() просто применяет колбэк к каждому элементу массива
+
+newTemps.forEach((temperature) => {
+  if (temperature.isHigh) {
+    console.log(
+      `Temperature ${temperature.degrees} was a record high last week!`
+    );
+  }
+});
+
+// получить подмножества массивов
+// метод map() возвращает массив той же длины,
+// чтобы получить подмножество массива применяется метод filter()
+// в подмассив войдут только те элементы, что прошли проверку условия в коллбэке
+const restaurants = [
+  { name: 'Cap City Diner', milesAway: 2.2 },
+  { name: 'Chop Shop', milesAway: 4.1 },
+  { name: 'Northstar Cafe', milesAway: 0.9 },
+  { name: 'City Tavern', milesAway: 0.5 },
+  { name: 'Shake Shack', milesAway: 5.3 },
+];
+
+const nameStarts_C = restaurants.filter((restaurant) =>
+  restaurant.name.startsWith('C')
+);
+console.log(nameStarts_C);
+// задаём условие выражением - в текущем объекте значение свойства 'name' начинается с заглавной буквы 'C'
+// если выражение вернёт значение true то элемент проходит фильтр в новый подмассив
+// подмассив не меняет начальный массив
+const nameStarts_Z = restaurants.filter((restaurant) =>
+  restaurant.name.startsWith('Z')
+);
+console.log(nameStarts_Z);
+// если нет элементов прошедших условие, вернётся пустой массив
+// по принципу возвращаемого значения true or false метод похож на some() and every()
+// если явно зададим true то просто скопируем весь массив
+const restaurantsCopy = restaurants.filter((restaurant) => true);
+console.log(restaurantsCopy);
+
+//условий может быть больше одного
+const results = restaurants.filter(
+  (restaurant) => restaurant.name.startsWith('C') && restaurant.milesAway < 3.0
+);
+console.log(results);
+// если надо вернуть лишь 1 элемент из всего массива то применяется метод find() - вернёт первое совпадение
+const firstMatch = restaurants.find(
+  (restaurant) => restaurant.name.startsWith('C') && restaurant.milesAway < 3.0
+);
+console.log('firstMatch', firstMatch);
+
+// вернёт первый эемент прошедший условие
+const anotherFirstMatch = restaurants.find(
+  (restaurant) =>
+    restaurant.name.toLowerCase().includes('city') && restaurant.milesAway < 3
+);
+console.log('anotherFirstMatch', anotherFirstMatch);
+// если соответствия условиям нет - вернёт undefined
+const noMatch = restaurants.find((restaurant) => false);
+console.log('noMatch', noMatch);
+
+//  метод reduce() вернёт значение любого типа , что нам надо
+const menuItems = [
+  { item: 'Blue Cheese Salad', price: 8 },
+  { item: 'Spicy Chicken Rigatoni', price: 18 },
+  { item: 'Ponzu Glazed Salmon', price: 23 },
+  { item: 'Philly Cheese Steak', price: 13 },
+  { item: 'Baked Italian Chicken Sub', price: 12 },
+  { item: 'Pan Seared Ribeye', price: 31 },
+];
+
+const total = menuItems.reduce((accumulator, menuItem) => {
+  console.log(`
+    accumulator: ${accumulator},
+    menu item price: ${menuItem.price}
+  `);
+  return accumulator + menuItem.price;
+}, 0);
+console.log(total);
+
+// создадим новый массив с помощью  reduce()  и удвоим значение каждого элемента
+const numbers = [1, 2, 3, 4, 5, 6, 1];
+
+const doubledNumbers = numbers.reduce((acc, num) => {
+  acc.push(num * 2);
+  return acc;
+}, []);
+console.log('doubled numbers', doubledNumbers);
+console.log('numbers', numbers);
+
+// перепишем reduce() на map() - частный случай reduce()
+const doubledMap = numbers.map((number) => number * 2);
+console.log('doubledoubledMap', doubledMap);
+
+// частный случай  filter()
+const moreThan3 = numbers.reduce((acc, num) => {
+  // acc.push(num * 2);
+  if (num > 3) {
+    acc.push(num);
+  }
+  return acc;
+}, []);
+
+console.log(moreThan3);
+// препишем на filter()
+const greaterNumbers = numbers.filter((num) => num > 3);
+console.log(greaterNumbers);
+
+// .push()  в одну строку не отрабатывает, меняем на  concat()
+const moreThan3_ternary = numbers.reduce(
+  (acc, num) => (num > 3 ? acc.concat(num) : acc),
+  []
+);
+console.log(moreThan3_ternary);
+
+// избегаем мутации начального массива с помощью  concat()
+const lunchMenuIdeas = ['Harvest Salad', 'Southern Fried Chicken'];
+
+const allMenuIdeas = lunchMenuIdeas.concat('Club Sandwich');
+allMenuIdeas.push('whatever');
+
+console.log(lunchMenuIdeas);
+console.log(allMenuIdeas);
+
+//  или клонируем массив с помощью оператора ...spread
+const cloneMenu = [...lunchMenuIdeas];
+cloneMenu.push('Red Hot Chilly Peppers');
+console.log(cloneMenu);
+
+const wrongThing = {thing: 'wrong'};
+// const whatHappens = [].concat(numbers, wrongThing);
+// const whatHappens = [...numbers, ...wrongThing];
+// console.log(whatHappens);
+
+numbers.push(wrongThing);
+console.log(numbers);
